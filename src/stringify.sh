64de7dcd7b24fb1c0dcd19daa7493d9f74bdb242
Danny Price <dan@thetelegraphic.com>
2023-02-17 13:55:03 +0800

    First version to compile shared object!

---
+++ src/stringify.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+./stringify  ArrayIndexer.cuh >  ArrayIndexer.cuh.jit
+./stringify  Complex.hpp >  Complex.hpp.jit
+./stringify  ShapeIndexer.cuh > ShapeIndexer.cuh.jit
+./stringify  Vector.hpp >  Vector.hpp.jit
+./stringify  IndexArray.cuh >  IndexArray.cuh.jit
+./stringify  int_fastdiv.h >  int_fastdiv.h.jit
